52c287d5a1b3a4a2e67a353a3f93f7d1daff077b
Vineethramchukka <vineethramchukka@gmail.com>
2024-11-11 01:41:24 +0530

    streamlit_app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import streamlit
-from predict_page import show_prediction
+import streamlit as st
 
-show_prediction()
+st.title("Test App")
+st.write("This is a simplified test to check if the deployment works.")
+
+if __name__ == "__main__":
+    st.write("The app is running.")
